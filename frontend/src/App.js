868bba21d97c7cd2efe633c744420a5357c25ded
haggau15 <haggau94@gmail.com>
2024-01-25 16:30:39 +0100

    Started on getting data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState,useEffect} from 'react';
+import './api'
+import {placesURL} from "./api";
 function App() {
+  const [places,setPlaces]=useState();
+
+  const getPlaces = async () =>{
+    try {
+      const response = await get(placesURL);
+      setPlaces(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(()=>{
+    getPlaces();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
